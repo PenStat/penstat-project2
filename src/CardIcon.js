953178557afcb308e6c5a1642ad4bfaff990d2bb
Pooja DebRoy <pvd5206@psu.edu>
2021-10-21 14:51:09 -0400

    classwork

--- src/CardIcon.js
+++ src/CardIcon.js
@@ -1,44 +1,49 @@
-import { html, css } from 'lit';
-import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
+// dependencies / things imported
+import { LitElement, html, css } from 'lit';
+import { SimpleColors } from "@lrnwebcomponents/simple-colors/simple-colors.js";
 
+// this is the base path to the assets calculated at run time
+// this ensures that assets are shipped correctly when building the demo
+// on github pages, or when people reuse assets outside your elements in production
+// because this won't change we can leverage as an internal variable without being
+// declared in properties. This let's us ship the icons while referencing them correctly
 const beaker = new URL('../assets/beaker.svg', import.meta.url).href;
 const lightbulb = new URL('../assets/lightbulb.svg', import.meta.url).href;
 const question = new URL('../assets/question.svg', import.meta.url).href;
-
+// EXPORT (so make available to other documents that reference this file) a class, that extends LitElement
+// which has the magic life-cycles and developer experience below added
 export class CardIcon extends SimpleColors {
-  // EXPORT (so make available to other documents that reference this file) a class, that extends LitElement
-  // which has the magic life-cycles and developer experience below added
   // a convention I enjoy so you can change the tag name in 1 place
   static get tag() {
     return 'card-icon';
   }
 
+  // HTMLElement life-cycle, built in; use this for setting defaults
   constructor() {
     super();
-    this.icon = lightbulb;
-    this.altText = 'A lightbulb icon';
+    this.icon = beaker;
   }
 
   // properties that you wish to use as data in HTML, CSS, and the updated life-cycle
   static get properties() {
     return {
-      icon: { type: String },
+      ...super.properties,
+      icon: {type: String},
     };
   }
 
+  // updated fires every time a property defined above changes
+  // this allows you to react to variables changing and use javascript to perform logic
   updated(changedProperties) {
     changedProperties.forEach((oldValue, propName) => {
-      if (propName === 'icon' && this[propName] === 'beaker') {
-        this.icon = beaker;
-        this.altText = 'A beaker icon';
+      if (propName === 'type' && this[propName] === 'science') {
+        this.myIcon = 'beaker';
       }
-      if (propName === 'icon' && this[propName] === 'lightbulb') {
-        this.icon = lightbulb;
-        this.altText = 'A lightbulb icon';
+      if (propName === 'type' && this[propName] === 'objective'){
+        this.myIcon = 'lightbulb';
       }
-      if (propName === 'icon' && this[propName] === 'question') {
-        this.icon = question;
-        this.altText = 'A question mark icon';
+      if (propName === 'type' && this[propName] === 'question'){
+        this.myIcon = 'question';
       }
     });
   }
@@ -65,31 +70,23 @@ export class CardIcon extends SimpleColors {
 
   // CSS - specific to Lit
   static get styles() {
-    return [
-      ...super.styles,
+    return [...super.styles,
       css`
-        :host {
-          display: block;
-        }
-        div {
-          corner-radius: 100%;
-          background-color: var(--simple-colors-default-theme-grey-6);
-          height: 200px;
-          width: 200px;
-        }
-        img {
-          height: 200px;
-          width: auto;
-        }
-      `,
+      :host {
+        display: block;
+        background-color: var(--simple-colors-default-theme-accent-8);
+        color: var(--simple-colors-default-theme-grey-1);
+      }
+      `
     ];
   }
 
   // HTML - specific to Lit
   render() {
     return html`
-      <div>
-        <img src="${this.icon}" alt="${this.altText}" />
+      <div class="outline">
+        <img href=${beaker} />
+        <img src="${this.icon}" alt=""/>
       </div>
     `;
   }
@@ -115,11 +112,14 @@ export class CardIcon extends SimpleColors {
       settings: {
         configure: [
           {
-            property: '',
-            title: '',
-            description: '',
-            inputMethod: '',
-            options: {},
+            property: 'type',
+            title: 'Type',
+            description: 'Identifies the card',
+            inputMethod: 'select',
+            options: {
+              science: 'Science',
+              math: 'Math',
+            },
           },
         ],
         advanced: [],
@@ -127,12 +127,14 @@ export class CardIcon extends SimpleColors {
       demoSchema: [
         {
           tag: CardIcon.tag,
-          properties: {},
-          content: '',
+          properties: {
+            type: 'science',
+          },
+          content:
+            "<p slot='header'>This tag renders in the header</p><ul><li>This renders</li><li>Below the tag</li></ul>",
         },
       ],
     };
   }
 }
-
-customElements.define(CardIcon.tag, CardIcon);
+window.customElements.define(CardIcon.tag, CardIcon);
