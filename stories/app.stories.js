d0ced7c274a3d237d7b27a50816ae6c2dfb441d7
elizabeth-delor <elizabeth.delor@gmail.com>
2021-11-02 14:41:43 -0400

    Storybook Fixes

--- stories/app.stories.js
+++ stories/app.stories.js
@@ -5,20 +5,25 @@ export default {
   title: 'Project two',
   component: 'learning-card',
   argTypes: {
-    type: { 
+    type: {
       control: 'radio',
-      options: ['science', 'objective', 'question']
+      options: ['science', 'objective', 'question'],
+    },
+    headingSlot: {
+      control: 'text',
+    },
+    subheadingSlot: {
+      control: 'text',
     },
   },
 };
 
-
-function Template({ type = "math", headingSlot, subheadingSlot, slot }) {
+function Template({ type = 'math', headingSlot, subheadingSlot, slot }) {
   return html`
     <learning-card type="${type}">
-    <div slot="header">${headingSlot}</div>
-    <div slot="subheader">${subheadingSlot}</div>
-    ${slot}
+      <div slot="header">${headingSlot}</div>
+      <div slot="subheader">${subheadingSlot}</div>
+      ${slot}
     </learning-card>
   `;
 }
@@ -28,21 +33,30 @@ ScienceCard.args = {
   type: 'science',
   headingSlot: html`<span>Unit 1</span>`,
   subheadingSlot: html`<span>Chem Connection</span>`,
-  slot: html`<ul><li>One</li><li>Two</li></ul>`
-}
+  slot: html`<ul>
+    <li>One</li>
+    <li>Two</li>
+  </ul>`,
+};
 
 export const ObjectiveCard = Template.bind({});
 ObjectiveCard.args = {
   type: 'objective',
   headingSlot: html`<span>Unit 1</span>`,
   subheadingSlot: html`<span>Learning Objectives</span>`,
-  slot: html`<ul><li>One</li><li>Two</li></ul>`
-}
+  slot: html`<ul>
+    <li>One</li>
+    <li>Two</li>
+  </ul>`,
+};
 
 export const QuestionCard = Template.bind({});
 QuestionCard.args = {
   type: 'question',
   headingSlot: html`<span>Unit 1</span>`,
   subheadingSlot: html`<span>Did You Know?</span>`,
-  slot: html`<ul><li>One</li><li>Two</li></ul>`
-}
+  slot: html`<ul>
+    <li>One</li>
+    <li>Two</li>
+  </ul>`,
+};
